c104ad13eab7e8401526763cfab9405c2062fe8e
isimongauz <isimongauz@gmcs.ru>
2016-10-07 15:27:48 +0300

    Option type added

---
+++ OneOf/Types/Option.cs
@@ -0,0 +1,34 @@
+﻿namespace OneOf.Types
+{
+    public class Option<T> : OneOfBase<T, None>
+    {
+        private Option(object value, int tag) : base(value, tag)
+        {
+        }
+
+        public T Value => ((IOneOf<T, None>) this).AsT0;
+
+        public bool HasValue => ((IOneOf<T, None>) this).IsT0;
+        public bool IsNone => ((IOneOf<T, None>)this).IsT1;
+
+        public static Option<T> None => OneOf.Types.None.Default;
+
+        public static implicit operator Option<T>(T value)
+        {
+            return new Option<T>(value, 0);    
+        }
+
+        public static implicit operator Option<T>(None none)
+        {
+            return new Option<T>(none, 1);
+        }
+    }
+
+    public static class Option
+    {
+        public static Option<T> ToOption<T>(this T value)
+        {
+            return value;
+        }
+    }
+}
